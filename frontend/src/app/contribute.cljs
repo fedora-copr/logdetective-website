6da87b183d8dce21aa240b3209477f495c66a5ff
Jakub Kadlcik <frostyx@email.cz>
2023-08-31 00:08:37 +0200

    frontend: after loading the page, fetch logs from backend

--- frontend/src/app/contribute.cljs
+++ frontend/src/app/contribute.cljs
@@ -2,56 +2,32 @@
   (:require [reagent.core :as r]
             [clojure.string :as str]
             [cljs.core.match :refer-macros [match]]
+            [lambdaisland.fetch :as fetch]
             [web.Range :refer [surround-contents]]))
-
-
-(def log (r/atom (str/join "\n" [
-"INFO: chroot_scan: 3 files copied to /var/lib/copr-rpmbuild/results/chroot_scan"
-"INFO: /var/lib/mock/fedora-37-x86_64-1692181166.067366/root/var/log/dnf.rpm.log"
-"/var/lib/mock/fedora-37-x86_64-1692181166.067366/root/var/log/dnf.librepo.log"
-"/var/lib/mock/fedora-37-x86_64-1692181166.067366/root/var/log/dnf.log"
-"ERROR:"
-"Exception(/var/lib/copr-rpmbuild/results/copr-rpmbuild-0.69-1.fc37.src.rpm)"
-"Config(fedora-37-x86_64) 0 minutes 28 seconds"
-"INFO: Results and/or logs in: /var/lib/copr-rpmbuild/results"
-"INFO: Cleaning up build root ('cleanup_on_failure=True')"
-"Start: clean chroot"
-"INFO: unmounting tmpfs."
-"Finish: clean chroot"
-"ERROR: Command failed:"
- "# /usr/bin/systemd-nspawn -q -M"
-"15916d5fd0524fe090a40cbc09f15279 -D"
-"/var/lib/mock/fedora-37-x86_64-bootstrap-1692181166.067366/root"
-"-a --capability=cap_ipc_lock --rlimit=RLIMIT_NOFILE=10240"
-"--capability=cap_ipc_lock"
-"--bind=/tmp/mock-resolv.0pyv_f8p:/etc/resolv.conf"
-"/var/lib/mock/fedora-37-x86_64-1692181166.067366/root/"
-"--releasever 37 --setopt=deltarpm=False"
-"--setopt=allow_vendor_change=yes --allowerasing"
-"--disableplugin=local --disableplugin=spacewalk"
-"--disableplugin=versionlock"
-"/var/lib/mock/fedora-37-x86_64-1692181166.067366/root//builddir/build/SRPMS/"
-"copr-rpmbuild-0.69-1.fc37.src.rpm"
-"--setopt=tsflags=nocontexts --setopt=tsflags=nocontexts"
-"--setopt=tsflags=nocontexts"
-"No matches found for the following disable plugin patterns: local, spacewalk, versionlock"
-"Copr repository                                  55 kB/s | 1.8 kB     00:00"
-"fedora                                          299 kB/s |  26 kB     00:00"
-"updates                                         403 kB/s |  19 kB     00:00"
-"No matching package to install: 'python3-specfile >= 0.21.0'"
-"Not all dependencies satisfied"
-"Error: Some packages could not be found."
-""
-"Copr build error: Build failed"
-])))
 
 
 (def active-tab (r/atom "backend.log"))
 (def snippets (r/atom nil))
 (def files (r/atom nil))
 
+(def backend-data (r/atom nil))
+(def log (r/atom nil))
+(def build-id (r/atom nil))
+(def build-id-title (r/atom nil))
+
+
+(defn fetch-logs []
+  (let [url "http://127.0.0.1:5020/frontend/fetch-logs/"]
+    (-> (fetch/get url {:accept :json :content-type :json})
+        (.then (fn [resp] (-> resp :body (js->clj :keywordize-keys true))))
+        (.then (fn [data]
+                 (reset! backend-data data)
+                 (reset! log (:log data))
+                 (reset! build-id (:build_id data))
+                 (reset! build-id-title (:build-id-title data)))))))
 
 (defn init-data []
+  (fetch-logs)
   (reset!
    files
    [{:name "builder-live.log"
@@ -168,7 +144,7 @@
   [:div {:class "col-3" :id "right-column"}
    [:div {:class "mb-3"}
     [:label {:class "form-label"} "Copr build ID"]
-    [:input {:type "text" :class "form-control" :value "123456"
+    [:input {:type "text" :class "form-control" :value (or @build-id "")
              :disabled true :readOnly true}]]
 
    [:div {:class "mb-3"}
