0cae37c14bb360502f1661fd1b01b8eb3749b557
Jakub Kadlcik <frostyx@email.cz>
2023-10-17 01:33:10 +0200

    frontend: oops, forgot to add this to the previous commit

---
+++ frontend/src/app/components/accordion.cljs
@@ -0,0 +1,41 @@
+(ns app.components.accordion
+ (:require
+  [reagent.core :as r]))
+
+
+(defn accordion-item [i item show?]
+  [:div {:class "accordion-item" :key i}
+   [:h2 {:class "accordion-header"}
+    [:button
+     {:class "accordion-button"
+      :type "button"
+      :data-bs-toggle "collapse"
+      :data-bs-target (str "#itemCollapse" i)
+      :aria-controls (str "itemCollapse" i)}
+     (str (:title item) " " (+ i 1))]]
+
+   [:div {:id (str "itemCollapse" i)
+          :class ["accordion-collapse collapse" (if show? "show" nil)]
+          :data-bs-parent "#accordionItems"
+          :data-index-number i}
+
+    [:div {:class "accordion-body"
+           :data-index-number i}
+     (:body item)
+     [:div {:class "accordion-buttons"
+            :data-index-number i}
+      (into [:<>] (:buttons item))]]]])
+
+(defn accordion-items [items]
+  (doall (for [enumerated-item (map-indexed list items)
+               :let [i (first enumerated-item)
+                     item (second enumerated-item)
+                     show? (= (first enumerated-item)
+                              (- (count items) 1))]
+               :when item]
+           (accordion-item i item show?))))
+
+(defn accordion [id items]
+  (when items
+    [:div {:class "accordion" :id id}
+     (accordion-items items)]))
