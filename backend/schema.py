8008096304c40581040ee6e3826c70895b576f3d
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-12-06 16:46:38 +0100

    backend: Introduce reviewing container logs
    - New container_file field added to schemas - serves the same purpose
    as spec_file, just for containerfiles.
    
    - New endpoint for feedbacks for container logs created
    /frontend/contribute/container
    
    - New provider for containers created, fetching only logs from URL
    for now
    
    Fixes #35

--- backend/schema.py
+++ backend/schema.py
@@ -1,20 +1,22 @@
 from typing import Optional
 
-from pydantic import AnyUrl, BaseModel
+from pydantic import AnyUrl, BaseModel, root_validator
 
 from backend.constants import BuildIdTitleEnum
 
 
-class LogSchema(BaseModel):
-    name: str
-    content: str
+def _check_spec_container_are_exclusively_mutual(_, values):
+    spec_file = values.get("spec_file")
+    container_file = values.get("container_file")
+    if spec_file and container_file:
+        raise ValueError("You can't specify both spec file and container file")
+
+    return values
 
 
-class SpecfileSchema(BaseModel):
-    # TODO: do we want to store spec_file separately
-    #  in file or store content in one file?
-    #  or the path means just its name?
-    # path: Path
+class NameContentSchema(BaseModel):
+    # TODO: do we want to store spec_file and container_file separately
+    #  in file or store content in one file? Or the path means just its name?
     name: str
     content: str
 
@@ -25,17 +27,25 @@ class ContributeResponseSchema(BaseModel):
      fetched data (logs, spec, ...) and are needed for user to give a feedback why
      build failed.
     """
+
     build_id: Optional[int]
     build_id_title: BuildIdTitleEnum
     build_url: AnyUrl
-    logs: list[LogSchema]
-    spec_file: SpecfileSchema
+    logs: list[NameContentSchema]
+    spec_file: Optional[NameContentSchema] = None
+    container_file: Optional[NameContentSchema] = None
+
+    # validators
+    _normalize_spec_and_container_file = root_validator(pre=True, allow_reuse=True)(
+        _check_spec_container_are_exclusively_mutual
+    )
 
 
 class SnippetSchema(BaseModel):
     """
     Snippet for log, each log may have 0 - many snippets.
     """
+
     # LINE_FROM:CHAR_FROM-LINE_TO:CHAR_TO
     # log_part: constr(regex=r"^\d+:\d+-\d+:\d+$")
     start_index: int
@@ -56,18 +66,25 @@ class SnippetSchema(BaseModel):
     #     return self._splitter(False)
 
 
-class FeedbackLogSchema(LogSchema):
+class FeedbackLogSchema(NameContentSchema):
     """
     Feedback from user per individual log with user's comment.
     """
+
     snippets: list[SnippetSchema]
 
 
 class _WithoutLogsSchema(BaseModel):
     username: Optional[str]
-    spec_file: SpecfileSchema
     fail_reason: str
     how_to_fix: str
+    spec_file: Optional[NameContentSchema] = None
+    container_file: Optional[NameContentSchema] = None
+
+    # validators
+    _normalize_spec_and_container_file = root_validator(pre=True, allow_reuse=True)(
+        _check_spec_container_are_exclusively_mutual
+    )
 
 
 class FeedbackInputSchema(_WithoutLogsSchema):
@@ -75,6 +92,7 @@ class FeedbackInputSchema(_WithoutLogsSchema):
     Contains data from users with reasons why build failed. It is sent from FE
      and contains only inputs from user + spec and logs content.
     """
+
     logs: list[FeedbackLogSchema]
 
 
@@ -83,18 +101,26 @@ class FeedbackSchema(_WithoutLogsSchema):
     This schema is the final structure as we decided to store our data in json file
      from users feedbacks.
     """
+
     logs: dict[str, FeedbackLogSchema]
 
 
-def schema_inp_to_out(inp: FeedbackInputSchema) -> FeedbackSchema:
+def schema_inp_to_out(
+    inp: FeedbackInputSchema, is_with_spec: bool = True
+) -> FeedbackSchema:
     parsed_log_schema = {}
     for log_schema in inp.logs:
         parsed_log_schema[log_schema.name] = log_schema
 
+    if is_with_spec:
+        spec_or_container = {"spec_file": inp.spec_file}
+    else:
+        spec_or_container = {"container_file": inp.container_file}
+
     return FeedbackSchema(
         username=inp.username,
-        spec_file=inp.spec_file,
         logs=parsed_log_schema,
         fail_reason=inp.fail_reason,
         how_to_fix=inp.how_to_fix,
+        **spec_or_container,
     )
