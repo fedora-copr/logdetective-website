274dc702639718e271e99eb3b817cce3429eb242
Jiri Podivin <jpodivin@redhat.com>
2025-08-26 15:13:38 +0200

    Moving production configuration into file
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

---
+++ files/gunicorn.conf.py
@@ -0,0 +1,14 @@
+# Configuration for production gunicorn server
+# pylint: skip-file
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2
+worker_class = "uvicorn.workers.UvicornWorker"
+max_requests = 100
+max_requests_jitter = 10
+threads = multiprocessing.cpu_count() * 2
+bind = "0.0.0.0:8080"
+cerfile = "/persistent/letsencrypt/live/logdetective.com/cert.pem"
+keyfile = "/persistent/letsencrypt/live/log-detective.com/privkey.pem"
+ca_certs = "/persistent/letsencrypt/live/log-detective.com/fullchain.pem"
+timeout = 1800
