ee0e970e646c64fea78d87a0ff828e409ae1c921
Jiri Podivin <jpodivin@redhat.com>
2025-11-10 08:27:27 +0100

    Rewording the contribution disclaimer to mention CC0
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- frontend/src/app/contribute.cljs
+++ frontend/src/app/contribute.cljs
@@ -230,8 +230,8 @@
    [:div {}
     [:label {:class "form-label"} "Ready to submit the results? "
      "By submitting this form, you agree that your input "
-     "can be used for teaching AI and that the data are "
-     "available under " [:a {:href "https://cdla.dev/permissive-2-0/"} "CDLA-Permissive-2.0"] " license."]
+     "will be covered by " [:a {:href "http://creativecommons.org/"} "CC0"] " and used to create dataset "
+     "released under " [:a {:href "https://cdla.dev/permissive-2-0/"} "CDLA-Permissive-2.0"] " license."]
     [:br]
     [:button {:type "submit"
               :class "btn btn-primary btn-lg"
