a896d50d3c0655c498030d185bc03297d6f2516c
Jan Matufka <jmatufka@redhat.com>
2026-05-18 20:09:56 +0200

    Use a pre-made archive in /download
    - this is to avoid hitting timeouts
    - archive is created every day at 23:45 (UTC)
    - older copies get removed upon successful creation
    
    Signed-off-by: Jan Matufka <jmatufka@redhat.com>

--- backend/src/spells.py
+++ backend/src/spells.py
@@ -6,7 +6,6 @@ import json
 import logging
 import os
 import shutil
-import tarfile
 import tempfile
 from contextlib import contextmanager
 from functools import lru_cache
@@ -41,26 +40,6 @@ def get_temporary_dir() -> Iterator[Path]:
         yield temp_dir
     finally:
         shutil.rmtree(temp_dir)
-
-
-def make_tar(name: str, sources: list[Path], destination: Path) -> Path:
-    """
-    Make tar from source path.
-
-    Args:
-        name: Name of the tar file
-        sources: Sources to be tarred
-        destination: Folder where to put tar file
-
-    Returns:
-        Path where to find a tar file.
-    """
-    tar_path = destination / name
-    with tarfile.open(tar_path, "w:gz") as tar_f:
-        for source in sources:
-            tar_f.add(source, arcname=f"results/{source.name}")
-
-    return tar_path
 
 
 def find_file_by_name(name: str, path: Path) -> Optional[Path]:
