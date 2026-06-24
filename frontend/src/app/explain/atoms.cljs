f061e04b11d0ae55a3f18f57a67a364bb00887f8
Jiri Podivin <jpodivin@redhat.com>
2026-06-24 13:25:59 +0200

    Turn atoms into plain strings
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- frontend/src/app/explain/atoms.cljs
+++ frontend/src/app/explain/atoms.cljs
@@ -5,8 +5,8 @@
 (def input-values (r/atom nil))
 (def input-errors (r/atom []))
 (def form (r/atom nil))
-(def ai-gen-disclaimer (r/atom "This explanation was provided by AI. Always review AI generated content prior to use."))
-(def mission-statement-prompt (r/atom
+(def ai-gen-disclaimer (str "This explanation was provided by AI. Always review AI generated content prior to use."))
+(def mission-statement-prompt
   (str
     "Improving RPM packaging experience by analyzing build "
-    "logs and explaining the failure in simple words.")))
+    "logs and explaining the failure in simple words."))
